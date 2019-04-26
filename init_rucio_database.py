387def824a1faefa11b6a5d7c157629e2db9626a
Gabriele <sucre.91@hotmail.it>
2019-04-26 18:57:36 +0200

    Rename of base script

--- init_rucio_database.py
+++ init_rucio_database.py
@@ -88,3 +88,6 @@ def init_rucio_database(cfg_file_path, echo=True, tests=False):
 
     # Put the database under version control
     command.stamp(alembic_cfg, "head")
+
+if __name__ == "__main__":
+    init_rucio_database("rucio-db.cfg")
