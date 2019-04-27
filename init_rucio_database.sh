f2436620430d2e33570cb3ada49c12f6f8d3a0e2
Gabriele <sucre.91@hotmail.it>
2019-04-27 18:04:11 +0200

    Recreating functional dirtree and removing git clone

--- init_rucio_database.sh
+++ init_rucio_database.sh
@@ -1,5 +1,5 @@
-git init rucio
-cd rucio
+git init rucio-git
+cd rucio-git
 git remote add -f origin https://github.com/rucio/rucio.git
 
 git config core.sparseCheckout true
@@ -9,8 +9,16 @@ echo "lib/rucio/db" >> .git/info/sparse-checkout
 
 git pull origin master
 
+mkdir ../rucio
+
+mv -r lib/rucio/* ../rucio
+
+cd ../rucio
+
 touch __init__.py
 
 cd ..
+
+rm -rf rucio-git
 
 python3 init_rucio_database.py
