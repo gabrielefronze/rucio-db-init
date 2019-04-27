d12ec0eff4a7095a2e4b3bcc5166c2ffe6fecc19
Gabriele <sucre.91@hotmail.it>
2019-04-27 17:57:32 +0200

    Using selective dynamic cloning of rucio repo

--- init_rucio_database.sh
+++ init_rucio_database.sh
@@ -1,1 +1,12 @@
+git init rucio
+cd rucio
+git remote add -f origin https://github.com/rucio/rucio.git
+
+git config core.sparseCheckout true
+
+echo "lib/rucio/common" >> .git/info/sparse-checkout
+echo "lib/rucio/db" >> .git/info/sparse-checkout
+
+git pull origin master
+
 python3 init_rucio_database.py
