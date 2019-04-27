2724f3c2e77c3e02e4b7af3d7c817eaa395dc239
Gabriele <sucre.91@hotmail.it>
2019-04-27 17:36:12 +0200

    Using standard config parser

--- init_rucio_database.py
+++ init_rucio_database.py
@@ -22,6 +22,7 @@ from sqlalchemy import create_engine, event
 
 # os.environ["RUCIO_HOME"]="/opt/rucio-db-init"
 
+from rucio.common.config import config_get
 from rucio.db.sqla.models import (SoftModelBase, Account, AccountAttrAssociation, Identity, IdentityAccountAssociation, Scope, DataIdentifier, DidMeta, 
                                     DeletedDataIdentifier, UpdatedDID, BadReplicas, BadPFNs, QuarantinedReplica, DIDKey, DIDKeyValueAssociation, DataIdentifierAssociation, 
                                     ConstituentAssociation, ConstituentAssociationHistory, DataIdentifierAssociationHistory, RSE, RSELimit, RSETransferLimit, RSEUsage, 
@@ -30,23 +31,23 @@ from rucio.db.sqla.models import (SoftModelBase, Account, AccountAttrAssociation
 from rucio.db.sqla.models import register_models
 from rucio.db.sqla.session import mysql_ping_listener, mysql_convert_decimal_to_float, psql_convert_decimal_to_float, _fk_pragma_on_connect, my_on_connect
 
-def get_config_parser(cfg_file_path):
-    """ Reads specified config file in a config parser.
-        :returns: config_parser
-    """
+# def get_config_parser(cfg_file_path):
+#     """ Reads specified config file in a config parser.
+#         :returns: config_parser
+#     """
 
-    config_parser = ConfigParser.SafeConfigParser(os.environ)
+#     config_parser = ConfigParser.SafeConfigParser(os.environ)
 
-    has_config = config_parser.read(cfg_file_path) == [cfg_file_path]
+#     has_config = config_parser.read(cfg_file_path) == [cfg_file_path]
 
-    if not has_config:
-        raise Exception('Could not load rucio configuration file rucio.cfg.'
-                        'Rucio looks in the following directories for a configuration file, in order:'
-                        '\n\t${RUCIO_HOME}/etc/rucio.cfg'
-                        '\n\t/opt/rucio/etc/rucio.cfg'
-                        '\n\t${VIRTUAL_ENV}/etc/rucio.cfg')
+#     if not has_config:
+#         raise Exception('Could not load rucio configuration file rucio.cfg.'
+#                         'Rucio looks in the following directories for a configuration file, in order:'
+#                         '\n\t${RUCIO_HOME}/etc/rucio.cfg'
+#                         '\n\t/opt/rucio/etc/rucio.cfg'
+#                         '\n\t${VIRTUAL_ENV}/etc/rucio.cfg')
 
-    return config_parser
+#     return config_parser
 
 def get_temp_engine(config_parser, echo=True):
     """ Creates a engine to a specific database.
@@ -76,14 +77,14 @@ def get_temp_engine(config_parser, echo=True):
     assert engine
     return engine
 
-def init_rucio_database(cfg_file_path, echo=True, tests=False):
+def init_rucio_database(echo=True, tests=False):
     """ Applies the schema to the database. Run this command once to build the database. """
 
-    config_parser = get_config_parser(cfg_file_path)
+    config_parser = config_get()
     alembic_cfg = config_parser.get('alembic', 'cfg')
 
     print("Applying the Rucio database schema to database endpoint: ", config_parser.get('database', 'default'))
-    print("Rucio configuration file: ", cfg_file_path)
+    print("Rucio configuration file: ", os.environ["RUCIO_HOME"],"/etc/rucio.cfg")
     print("Alembic.ini configuration file: ", alembic_cfg)
 
     engine = get_temp_engine(config_parser, echo=echo)
@@ -93,4 +94,4 @@ def init_rucio_database(cfg_file_path, echo=True, tests=False):
     # command.stamp(alembic_cfg, "head")
 
 if __name__ == "__main__":
-    init_rucio_database('rucio-db.cfg')
+    init_rucio_database()
